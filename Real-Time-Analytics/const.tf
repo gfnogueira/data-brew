9d22d6c2885d2155f9c542abcff2f51b1a633085
Guilherme Nogueira <nogueira.gfn@gmail.com>
2025-05-10 20:17:13 -0300

    update org account name

--- Real-Time-Analytics/const.tf
+++ Real-Time-Analytics/const.tf
@@ -1,5 +1,5 @@
 locals {
-  organization_name         = "nogueira"
+  organization_name         = "nogs"
   account_name              = "pocs"
   account_region            = "us-east-1"
   organization_account_name = format("%s-%s", local.organization_name, local.account_name)
