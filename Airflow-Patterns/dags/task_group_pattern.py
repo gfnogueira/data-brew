9f3ed30ea3fe527e12075f3eb181560d225bf36f
Guilherme Nogueira <nogueira.gfn@gmail.com>
2025-05-10 20:15:24 -0300

    update dag - task-group test

--- Airflow-Patterns/dags/task_group_pattern.py
+++ Airflow-Patterns/dags/task_group_pattern.py
@@ -3,7 +3,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.utils.task_group import TaskGroup
 from datetime import datetime
 
-with DAG("task_group_example", start_date=datetime(2023, 1, 1), schedule_interval="@daily", catchup=False) as dag:
+with DAG("task_group", start_date=datetime(2023, 1, 1), schedule_interval="@daily", catchup=False) as dag:
     start = DummyOperator(task_id="start")
 
     with TaskGroup("extract_group") as extract:
